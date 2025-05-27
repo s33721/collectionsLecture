6bcb694bc2c785e39a59962e9d40c7a1ec17de70
s33721 <s33721@pjwstk.edu.pl>
2025-05-27 13:02:26 +0200

    collections

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
-        Student s1 = new Student("John", "Doe");
-        Student s2 = new Student("Jane", "Doe");
-        Student s3 = new Student("Jack", "Doe");
+        Student s1 = new Student("John", "Doe", 40);
+        Student s2 = new Student("Jane", "Doe", 52);
+        Student s3 = new Student("Jack", "Doe", 20);
         students.add(s1);
         students.add(s2);
         students.add(s3);
-        Student studentIWantToFindAndRemove = new Student("John", "Doe");
+        Student studentIWantToFindAndRemove = new Student("John", "Doe", 40);
         if (studentIWantToFindAndRemove.equals(s1)) {
             System.out.println("The student is the same");
         }
@@ -23,7 +24,7 @@ public class Main {
         studentSet.add(s2);
         studentSet.add(s3);
         System.out.println(studentSet.size()); //3 because hashset dont accept duplicates, arraylist does
-        if(studentSet.contains(studentIWantToFindAndRemove)); //faster than in arraylist
+        if (studentSet.contains(studentIWantToFindAndRemove)) ; //faster than in arraylist
         //hashcode doesnt store in order
         for (Student student : studentSet) {
             System.out.println(student);
@@ -34,16 +35,22 @@ public class Main {
         map.put("s4561", s2);
         map.put("s4562", s3);
         Student s = map.get("s1234");
+        //collections, works only if student is comparable
+        Collections.sort(students);
+        for (Student student : students) {
+            System.out.println(student.getFirstName() + " " + student.getLastName());
+        }
 
-
-
+        System.out.println("============");
+        System.out.println("experiment");
+        System.out.println("============");
         //experiment
         boolean works = students.remove(studentIWantToFindAndRemove);
-        if (works){
-            System.out.println("Student removed " + studentIWantToFindAndRemove.toString());
+        if (works) {
+            System.out.println("Student removed " + studentIWantToFindAndRemove.getFirstName() + " " + studentIWantToFindAndRemove.getLastName());
         }
         for (Student student : students) {
-            System.out.println(student);
+            System.out.println(student.getFirstName() + " " + student.getLastName());
         }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,12 @@
-public class Student {
+public class Student implements Comparable<Student> {
     private String firstName;
     private String lastName;
+    private int age;
 
-    public Student(String firstName, String lastName) {
+    public Student(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
     }
 
     @Override
@@ -16,9 +18,39 @@ public class Student {
 
         return false;
     }
+
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return firstName.hashCode() + lastName.hashCode();
+    }
+
+    @Override
+    public int compareTo(Student someStudent) {
+        //-1 if age is smaller than another student
+        //0 if ages are equal
+        //+1 if age is bigger than another student
+        if (age < someStudent.age) {
+            return -1;
+        } else if (age == someStudent.age) {
+            return 0;
+        }
+        return 1;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
 
